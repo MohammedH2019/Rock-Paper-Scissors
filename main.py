0ad6ede00d67af120b1d0091a88aaf482ef30bf7
MohammedH2019 <MohammedHoche1@gmail.com>
2022-09-26 16:12:38 +0100

    inital commit for the rock,paper,Scissors game in Python

---
+++ main.py
@@ -0,0 +1,38 @@
+# A python application console that allow a user to play Rock,Paper and Scissors against a computer
+from random import randint
+
+play_option = ['Rock','Paper','Scissors']
+
+#assign a random play_option to the computer
+computer = play_option[randint(0,2)] # randint returns a random int value from 0-2 from the play_option list
+
+
+# By default set the Player to False. I.e we will only exist the loop when the player is set to True
+player = False
+while player == False:
+    #set player to True
+    player = input('Enter Rock, Paper or Scissors?')
+    if player == computer: # if player chooses and computer have the same play - it results into a tie game
+        print('TIE!')
+    elif player == 'Rock': # if the player chooses Rock, the computer has only two choices Paper and Scissors
+        if computer == 'Paper':
+            print('You lose!', computer, 'covers', player)
+        else:
+            print('you win!', player, 'smashes', computer)
+    elif player == 'Scissors': # if the player chooses Scissors , the computer has only two options Rock and Paper
+        if computer == 'Rock':
+            print('You lose', computer, 'smahes', player)
+        else:
+           print('You win!',player,'smashes', computer)
+    elif player == 'Paper': # if the player chooses Paper , the computer has only two options Scissors and Rock
+        if computer == 'Scissors':
+            print('You lose!', computer, 'cuts', player)
+        else:
+            print('You win', player, 'covers', computer)     
+    else:
+        print('Not a valid Play - check your input') # if input arg does not meet any of the criteria then we print this
+    player = False # To continue the game after first try we set the player value back to False
+    computer = play_option[randint(0,2)] # update the computer value
+
+
+             
